8bb996fc19977c2d52db473cbbb51af2149f7216
AlexL <alefranc.pro@mailo.com>
2025-02-08 18:42:00 +0100

    wip p1

--- p1/scripts/install-k3s-server.sh
+++ p1/scripts/install-k3s-server.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --flannel-backend none --node-ip "192.168.56.110" --bind-address "192.168.56.110" --agent-token 12345" sh -s -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip "192.168.56.110" --flannel-iface eth1 --bind-address "192.168.56.110" --agent-token 12345" sh -s -
